882cf6c7d8aadc0a96fdf0a7ef31e774c78e7515
Moon <1tjsen@naver.com>
2025-06-04 15:21:22 +0900

    feat: Cleric에 selfAid 추가

--- lib/assignments/2025-06-04/cleric.dart
+++ lib/assignments/2025-06-04/cleric.dart
@@ -7,4 +7,15 @@ class Cleric {
   int mp;
 
   Cleric({required this.name, this.hp = maxHp, this.mp = maxMp});
+
+  void selfAid() {
+    if (mp < 5) {
+      print("MP가 부족합니다. 현재 MP: $mp");
+      return;
+    }
+
+    mp -= 5;
+    hp = maxHp;
+    print("$name의 HP가 최대로 회복되었습니다. 현재 HP: $hp, MP: $mp");
+  }
 }
