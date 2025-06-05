baa5a3ca68802b0750949d57758e1e711699abc3
Moon <1tjsen@naver.com>
2025-06-05 14:01:28 +0900

    chore: 매직 넘버 제거

--- lib/assignments/2025-06-04/cleric.dart
+++ lib/assignments/2025-06-04/cleric.dart
@@ -3,6 +3,9 @@ import 'dart:math';
 class Cleric {
   static const int maxHp = 50;
   static const int maxMp = 10;
+  static const int selfAidMp = 5;
+  static const int didNotPray = 0;
+  static const int randomNextInt = 3;
 
   String name;
   int hp;
@@ -11,28 +14,28 @@ class Cleric {
   Cleric({required this.name, this.hp = maxHp, this.mp = maxMp});
 
   void selfAid() {
-    if (mp < 5) {
+    if (mp < selfAidMp) {
       print('MP가 부족합니다. 현재 MP: $mp');
       return;
     }
 
-    mp -= 5;
+    mp -= selfAidMp;
     hp = maxHp;
     print('$name의 HP가 최대로 회복되었습니다. 현재 HP: $hp, MP: $mp');
   }
 
   int pray(int seconds) {
-    if (seconds == 0) {
+    if (seconds == didNotPray) {
       print('pray를 실행하지 않습니다. 현재 MP: $mp');
-      return 0;
+      return didNotPray;
     }
 
     if (mp == maxMp) {
       print('MP가 이미 최대입니다. 현재 MP: $mp');
-      return 0;
+      return didNotPray;
     }
 
-    final weight = Random().nextInt(3);
+    final weight = Random().nextInt(randomNextInt);
     int restoredMp = weight + seconds;
 
     if (restoredMp + mp > maxMp) {
