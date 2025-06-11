4b20a9fc5148932a862350d0a3fd293f8233850b
Moon <1tjsen@naver.com>
2025-06-11 16:44:58 +0900

    test: Person 테스트 추가

---
+++ test/assignments/person_test.dart
@@ -0,0 +1,33 @@
+import 'package:modu_3_dart_study/assignments/person.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const String defaultName = 'name';
+  const int defaultBirthYear = 1996;
+
+  final int thisYear = DateTime.now().year;
+
+  group('constructor', () {
+    test('When create a person, the name, birthYear are given value.', () {
+      // given
+
+      // when
+      final person = Person(defaultName, birthYear: defaultBirthYear);
+
+      // then
+      expect(person.name, defaultName);
+      expect(person.birthYear, defaultBirthYear);
+    });
+  });
+
+  group('age', () {
+    test('When create a person, the age == thisYear - birthYear.', () {
+      // given
+      final person = Person(defaultName, birthYear: defaultBirthYear);
+
+      // when
+      // then
+      expect(person.age, thisYear - defaultBirthYear);
+    });
+  });
+}
