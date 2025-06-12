c50509892a497b345cf86d44845402c17894d6d2
Moon <1tjsen@naver.com>
2025-06-12 15:40:53 +0900

    feat: Slime 클래스 추가

---
+++ lib/assignments/slime.dart
@@ -0,0 +1,17 @@
+import 'package:modu_3_dart_study/assignments/hero.dart';
+
+class Slime {
+  static const maxHp = 50;
+  static const damage = 10;
+
+  final String suffix;
+  final int hp;
+
+  Slime(this.suffix, {this.hp = maxHp});
+
+  void attack(Hero hero) {
+    print('슬라임$suffix이/가 ${hero.name}을/를 공격했다.');
+    print('$damage의 데미지.');
+    hero.decreaseHp(damage);
+  }
+}
