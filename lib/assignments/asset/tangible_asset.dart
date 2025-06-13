85c56e5690b42bae81f52c343ee3a72ecc23004c
Moon <1tjsen@naver.com>
2025-06-13 16:24:35 +0900

    refactor: TangibleAsset이 Thing 구현
    - 무게는 음수를 가질 수 없음

--- lib/assignments/asset/tangible_asset.dart
+++ lib/assignments/asset/tangible_asset.dart
@@ -1,7 +1,27 @@
 import 'package:modu_3_dart_study/assignments/asset/asset.dart';
+import 'package:modu_3_dart_study/assignments/asset/thing.dart';
 
-abstract class TangibleAsset extends Asset {
+abstract class TangibleAsset extends Asset implements Thing {
   final String color;
 
-  TangibleAsset(super.name, {required super.price, required this.color});
+  double _weight;
+
+  TangibleAsset(
+    super.name, {
+    required super.price,
+    required this.color,
+    required double weight,
+  }) : _weight = weight;
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double value) {
+    if (value < Thing.minWeight) {
+      throw Exception('무게는 ${Thing.minWeight}보다 커야 합니다.');
+    }
+
+    _weight = value;
+  }
 }
--- lib/assignments/asset/thing.dart
+++ lib/assignments/asset/thing.dart
@@ -1,4 +1,6 @@
 abstract interface class Thing {
+  static const double minWeight = 0;
+
   double get weight;
   set weight(double value);
 }
