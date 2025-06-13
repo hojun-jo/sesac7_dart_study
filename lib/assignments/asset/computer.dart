272736489a40c8b6d320b5234b0f5540c0038498
Moon <1tjsen@naver.com>
2025-06-13 14:18:29 +0900

    feat: Computer 클래스 추가

---
+++ lib/assignments/asset/computer.dart
@@ -0,0 +1,13 @@
+class Computer {
+  final String name;
+  final int price;
+  final String color;
+  final String makerName;
+
+  Computer(
+    this.name, {
+    required this.price,
+    required this.color,
+    required this.makerName,
+  });
+}
