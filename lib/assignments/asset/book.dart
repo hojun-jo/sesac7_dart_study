56480efac587cd7b8f71118f74648c8ddd9b1e0d
Moon <1tjsen@naver.com>
2025-06-13 14:12:34 +0900

    feat: Book 클래스 추가

---
+++ lib/assignments/asset/book.dart
@@ -0,0 +1,13 @@
+class Book {
+  final String name;
+  final int price;
+  final String color;
+  final String isbn;
+
+  Book(
+    this.name, {
+    required this.price,
+    required this.color,
+    required this.isbn,
+  });
+}
