d34ee8017515924d48b3478a786d734e3245dbac
Moon <1tjsen@naver.com>
2025-06-13 16:51:16 +0900

    test: Computer 테스트 추가
    - 생성자
    - weight setter

---
+++ test/assignments/asset/computer_test.dart
@@ -0,0 +1,62 @@
+import 'package:modu_3_dart_study/assignments/asset/computer.dart';
+import 'package:modu_3_dart_study/assignments/asset/thing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const String name = 'Computer';
+  const int price = 1000000;
+  const String color = 'Black';
+  const String makerName = 'Apple';
+  const double weight = 1000;
+
+  late Computer computer;
+
+  setUp(() {
+    computer = Computer(
+      name,
+      price: price,
+      color: color,
+      makerName: makerName,
+      weight: weight,
+    );
+  });
+
+  group('constructor', () {
+    test(
+      'When a book is created with name, price, color, isbn, and weight, it contains the given values.',
+      () {
+        // given
+        // when
+
+        // then
+        expect(computer.name, name);
+        expect(computer.price, price);
+        expect(computer.color, color);
+        expect(computer.makerName, makerName);
+        expect(computer.weight, weight);
+      },
+    );
+  });
+
+  group('weight', () {
+    test('Throws an exception if the weight is less than zero.', () {
+      // given
+      final lessThanZero = Thing.minWeight - 1;
+
+      // when
+      // then
+      expect(() => computer.weight = lessThanZero, throwsException);
+    });
+
+    test('If the weight is greater than zero, it has that value.', () {
+      // given
+      final moreThanZero = Thing.minWeight + 1;
+
+      // when
+      computer.weight = moreThanZero;
+
+      // then
+      expect(computer.weight, moreThanZero);
+    });
+  });
+}
