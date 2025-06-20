beca3808a4d3b5c10528a3a7fb57096fd26a73c8
Moon <1tjsen@naver.com>
2025-06-20 17:11:53 +0900

    refactor: StrongBox 수정
    구현 내용
    - KeyType, _openTryCount 필드 추가
    - get 시도 횟수가 key.tryLimit에 도달해야 item 리턴

--- lib/assignments/strong_box/strong_box.dart
+++ lib/assignments/strong_box/strong_box.dart
@@ -1,9 +1,24 @@
+import 'package:modu_3_dart_study/assignments/strong_box/key_type.dart';
+
 class StrongBox<T> {
+  final KeyType key;
+
   T? item;
+
+  int _openTryCount = 0;
+
+  StrongBox({required this.key});
 
   void put(T item) {
     this.item = item;
   }
 
-  T? get() => item;
+  T? get() {
+    if (_openTryCount < key.tryLimit) {
+      _openTryCount++;
+      return null;
+    }
+
+    return item;
+  }
 }
