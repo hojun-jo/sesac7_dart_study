fbda31670ee1176de6fd345805bf62a3d64dece8
Moon <1tjsen@naver.com>
2025-06-20 18:47:27 +0900

    test: Word 테스트 추가

---
+++ test/assignments/word_test.dart
@@ -0,0 +1,99 @@
+import 'package:modu_3_dart_study/assignments/word.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const String sampleWord = 'sample!';
+  const String upperCaseWord = 'SAMPLE!';
+  const int vowelIndex = 1;
+  const int consonantIndex = 0;
+  const int specialCharacterIndex = 6;
+  const int outOfIndex = sampleWord.length;
+
+  late Word word;
+
+  setUp(() {
+    word = Word(sampleWord);
+  });
+
+  group('isVowel', () {
+    test('The vowelIndex of sampleWord has isVowel true.', () {
+      // given
+      // when
+      // then
+      expect(word.isVowel(vowelIndex), isTrue);
+    });
+
+    test('The consonantIndex of sampleWord has isVowel false.', () {
+      // given
+      // when
+      // then
+      expect(word.isVowel(consonantIndex), isFalse);
+    });
+
+    test('The outOfIndex of sampleWord has isVowel throws exception.', () {
+      // given
+      final word = Word(sampleWord);
+
+      // when
+      // then
+      expect(() => word.isVowel(outOfIndex), throwsException);
+    });
+
+    test('It works fine even if word is capitalized.', () {
+      // given
+      final word = Word(upperCaseWord);
+
+      // when
+      // then
+      expect(word.isVowel(vowelIndex), isTrue);
+    });
+
+    test('The specialCharacterIndex of sampleWord has isVowel false.', () {
+      // given
+      // when
+      // then
+      expect(word.isVowel(specialCharacterIndex), isFalse);
+    });
+  });
+
+  group('isConsonant', () {
+    test('The vowelIndex of sampleWord has isConsonant false.', () {
+      // given
+      // when
+      // then
+      expect(word.isConsonant(vowelIndex), isFalse);
+    });
+
+    test('The consonantIndex of sampleWord has isConsonant true.', () {
+      // given
+      // when
+      // then
+      expect(word.isConsonant(consonantIndex), isTrue);
+    });
+
+    test('The outOfIndex of sampleWord has isConsonant throws exception.', () {
+      // given
+      final word = Word(sampleWord);
+
+      // when
+      // then
+      expect(() => word.isConsonant(outOfIndex), throwsException);
+    });
+
+    test('It works fine even if word is capitalized.', () {
+      // given
+      final word = Word(upperCaseWord);
+
+      // when
+      // then
+      expect(word.isConsonant(vowelIndex), isFalse);
+    });
+
+    test('The specialCharacterIndex of sampleWord has isConsonant false.', () {
+      // given
+      // when
+      // then
+      expect(word.isConsonant(specialCharacterIndex), isFalse);
+    });
+  });
+}
