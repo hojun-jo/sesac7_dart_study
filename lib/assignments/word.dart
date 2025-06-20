38e88b0301459d5212d7f9810be02223d7e5803c
Moon <1tjsen@naver.com>
2025-06-20 18:29:00 +0900

    fix: Word의 인덱스 범위 내에 있도록 예외 추가

--- lib/assignments/word.dart
+++ lib/assignments/word.dart
@@ -1,10 +1,17 @@
 class Word {
   static const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-  String word = '';
+  final String _word;
+
+  Word(String word) : _word = word;
 
   bool isVowel(int i) {
-    return vowels.contains(word.substring(i, i + 1));
+    if (i >= _word.length) {
+      throw Exception('인덱스는 글자 길이보다 작은 값이 필요합니다.');
+    }
+
+    final target = _word.toLowerCase().substring(i, i + 1);
+    return vowels.contains(target);
   }
 
   bool isConsonant(int i) {
