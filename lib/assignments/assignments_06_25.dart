de911955e2fc8c255ea93bde266892631d89a3eb
Moon <1tjsen@naver.com>
2025-06-25 18:55:01 +0900

    feat: fromJson, toJson 연습문제 추가

--- lib/assignments/assignments_06_25.dart
+++ lib/assignments/assignments_06_25.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:modu_3_dart_study/assignments/company/department.dart';
+import 'package:modu_3_dart_study/assignments/company/employee.dart';
+
 void main() {
+  // try catch 연습문제
   final numString = '10.5';
   int num = 0;
 
@@ -9,4 +16,23 @@ void main() {
   }
 
   print(num);
+
+  // fromJson, toJson 연습문제
+  final employee = Employee(name: '홍길동', age: 41);
+  final department = Department(name: '총무부', leader: employee);
+
+  final json = department.toJson();
+  print('json: $json');
+  final jsonString = jsonEncode(json);
+  print('jsonString: $jsonString');
+
+  final file = File('assets/company.txt');
+  file.writeAsStringSync(jsonString);
+  final readJsonString = file.readAsStringSync();
+  print('readJsonString: $readJsonString');
+  final Map<String, dynamic> readJson = jsonDecode(readJsonString);
+  print('readJson: $readJson');
+
+  final readDepartment = Department.fromJson(readJson);
+  print(readDepartment);
 }
