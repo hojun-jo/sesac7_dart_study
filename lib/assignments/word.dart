b771538243379ec45436dd20fe978e84a5a086ba
Moon <1tjsen@naver.com>
2025-06-25 12:02:51 +0900

    fix: Word 내의 static const Set 내용이 수정 가능한 문제 수정

--- lib/assignments/word.dart
+++ lib/assignments/word.dart
@@ -1,43 +1,21 @@
 class Word {
   static const int minIndex = 0;
-  static const Set<String> vowels = {'a', 'e', 'i', 'o', 'u'};
-  static const Set<String> consonant = {
-    'b',
-    'c',
-    'd',
-    'f',
-    'g',
-    'h',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  };
+  static const String vowels = 'aeiou';
+  static const String consonant = 'bcdfghjklmnpqrstvwxyz';
 
   final String _word;
 
   Word(String word) : _word = word;
 
   bool isVowel(int i) {
-    return _isContainWord(index: i, words: vowels);
+    return _isContainWord(index: i, alphabets: vowels);
   }
 
   bool isConsonant(int i) {
-    return _isContainWord(index: i, words: consonant);
+    return _isContainWord(index: i, alphabets: consonant);
   }
 
-  bool _isContainWord({required int index, required Set<String> words}) {
+  bool _isContainWord({required int index, required String alphabets}) {
     if (index < minIndex) {
       throw Exception('인덱스는 $minIndex보다 큰 값이 필요합니다.');
     }
@@ -45,7 +23,7 @@ class Word {
       throw Exception('인덱스는 글자 길이보다 작은 값이 필요합니다.');
     }
 
-    final target = _word.toLowerCase()[index];
-    return words.contains(target);
+    final target = _word[index].toLowerCase();
+    return alphabets.contains(target);
   }
 }
