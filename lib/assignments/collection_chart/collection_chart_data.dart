a146b86401120d53fd5765ab0085acde8218d31f
Moon <1tjsen@naver.com>
2025-06-26 17:03:56 +0900

    feat: CollectionChartData 클래스 추가
    구현 내용
    - fromJson
    - toJson
    - hashCode
    - ==
    - toString

---
+++ lib/assignments/collection_chart/collection_chart_data.dart
@@ -0,0 +1,48 @@
+import 'package:modu_3_dart_study/assignments/collection_chart/collection_sale_price.dart';
+
+class CollectionChartData {
+  static const String collectionNameString = 'collectionName';
+  static const String collectionSalePriceString = 'collectionSalePrice';
+
+  final String? collectionName;
+  final List<CollectionSalePrice>? collectionSalePrice;
+
+  CollectionChartData({
+    required this.collectionName,
+    required this.collectionSalePrice,
+  });
+
+  CollectionChartData.fromJson(Map<String, dynamic> json)
+    : collectionName = json[collectionNameString],
+      collectionSalePrice =
+          (json[collectionSalePriceString] == null
+                  ? null
+                  : json[collectionSalePriceString] as List)
+              ?.map((e) => e as Map<String, dynamic>)
+              .map((e) => CollectionSalePrice.fromJson(e))
+              .toList();
+
+  Map<String, dynamic> toJson() {
+    return {
+      collectionNameString: collectionName,
+      collectionSalePriceString: collectionSalePrice
+          ?.map((e) => e.toJson())
+          .toList(),
+    };
+  }
+
+  @override
+  int get hashCode => Object.hash(collectionName, collectionSalePrice);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CollectionChartData &&
+          collectionName == other.collectionName &&
+          collectionSalePrice == other.collectionSalePrice;
+
+  @override
+  String toString() {
+    return 'CollectionChartData{$collectionNameString: $collectionName, $collectionSalePriceString: $collectionSalePrice}\n';
+  }
+}
