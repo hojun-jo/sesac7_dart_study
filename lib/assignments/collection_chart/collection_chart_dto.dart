fc779a068a83575694ec1ef09b8068583b8988f8
Moon <1tjsen@naver.com>
2025-06-26 18:05:23 +0900

    feat: CollectionChartDTO 클래스 추가
    구현 내용
    - fromJson
    - toJson
    - hashCode
    - ==
    - toString

---
+++ lib/assignments/collection_chart/collection_chart_dto.dart
@@ -0,0 +1,43 @@
+import 'package:collection/collection.dart';
+import 'package:modu_3_dart_study/assignments/collection_chart/collection_chart_data.dart';
+
+class CollectionChartDTO {
+  static const String collectionChartDataListString = 'collectionChartDataList';
+
+  final List<CollectionChartData>? collectionChartDataList;
+
+  CollectionChartDTO({required this.collectionChartDataList});
+
+  CollectionChartDTO.fromJson(Map<String, dynamic> json)
+    : collectionChartDataList = json[collectionChartDataListString] == null
+          ? null
+          : (json[collectionChartDataListString] as List)
+                .map((e) => e as Map<String, dynamic>)
+                .map((e) => CollectionChartData.fromJson(e))
+                .toList();
+
+  Map<String, dynamic> toJson() {
+    return {
+      collectionChartDataListString: collectionChartDataList
+          ?.map((e) => e.toJson())
+          .toList(),
+    };
+  }
+
+  @override
+  int get hashCode => collectionChartDataList.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CollectionChartDTO &&
+          ListEquality().equals(
+            collectionChartDataList,
+            other.collectionChartDataList,
+          );
+
+  @override
+  String toString() {
+    return 'CollectionChartObject{$collectionChartDataListString: $collectionChartDataList}\n';
+  }
+}
