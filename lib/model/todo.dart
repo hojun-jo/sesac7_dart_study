526165583139d187eb1ab00c9de7f7c8226d18e6
Moon <1tjsen@naver.com>
2025-07-02 14:03:09 +0900

    feat: Todo 모델 추가

---
+++ lib/model/todo.dart
@@ -0,0 +1,49 @@
+class Todo {
+  final num? userId;
+  final num? id;
+  final String? title;
+  final bool? completed;
+
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      completed: json['completed'],
+    );
+  }
+
+  Todo copyWith({
+    num? userId,
+    num? id,
+    String? title,
+    bool? completed,
+  }) {
+    return Todo(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+    );
+  }
+
+  @override
+  int get hashCode => Object.hash(userId, id, title, completed);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Todo &&
+            userId == other.userId &&
+            id == other.id &&
+            title == other.title &&
+            completed == other.completed;
+  }
+}
