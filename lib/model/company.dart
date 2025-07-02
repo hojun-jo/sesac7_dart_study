fefa2c30beb5ef6ff010bce817cb69636a57537b
Moon <1tjsen@naver.com>
2025-07-02 16:05:20 +0900

    feat: Company 모델 추가

---
+++ lib/model/company.dart
@@ -0,0 +1,51 @@
+class Company {
+  final String? name;
+  final String? catchPhrase;
+  final String? bs;
+
+  Company({
+    required this.name,
+    required this.catchPhrase,
+    required this.bs,
+  });
+
+  factory Company.fromJson(Map<String, dynamic> json) {
+    return Company(
+      name: json['name'],
+      catchPhrase: json['catchPhrase'],
+      bs: json['bs'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'catchPhrase': catchPhrase,
+      'bs': bs,
+    };
+  }
+
+  Company copyWith({
+    String? name,
+    String? catchPhrase,
+    String? bs,
+  }) {
+    return Company(
+      name: name ?? this.name,
+      catchPhrase: catchPhrase ?? this.catchPhrase,
+      bs: bs ?? this.bs,
+    );
+  }
+
+  @override
+  int get hashCode => Object.hash(name, catchPhrase, bs);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Company &&
+            name == other.name &&
+            catchPhrase == other.catchPhrase &&
+            bs == other.bs;
+  }
+}
