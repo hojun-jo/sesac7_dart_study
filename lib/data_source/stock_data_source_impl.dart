1071c67cbf2003b9e3894190690f9a0046204f47
Moon <1tjsen@naver.com>
2025-07-02 19:09:24 +0900

    feat: StockDataSourceImpl 구현체 추가

---
+++ lib/data_source/stock_data_source_impl.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:modu_3_dart_study/data_source/stock_data_source.dart';
+import 'package:modu_3_dart_study/model/stock_listing.dart';
+
+class StockDataSourceImpl implements StockDataSource {
+  @override
+  Future<List<StockListing>> getStockListings() async {
+    final file = File('assets/listing_status.csv');
+    final csvString = await file.readAsString();
+    return csvString
+        .trim()
+        .split('\n')
+        .map((e) => e.replaceAll('\r', ''))
+        .map(StockListing.fromCsv)
+        .where((e) => e.name != null && e.name != '' && e.name != 'name')
+        .toList();
+  }
+}
