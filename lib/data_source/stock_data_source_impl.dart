df0bb66091d9e85d7260eeaa389ea8f2d9421edf
Moon <1tjsen@naver.com>
2025-07-02 19:40:44 +0900

    refactor: StockDataSourceImpl에 _csvLabelName 상수 추가

--- lib/data_source/stock_data_source_impl.dart
+++ lib/data_source/stock_data_source_impl.dart
@@ -4,6 +4,8 @@ import 'package:modu_3_dart_study/data_source/stock_data_source.dart';
 import 'package:modu_3_dart_study/model/stock_listing.dart';
 
 class StockDataSourceImpl implements StockDataSource {
+  static const _csvLabelName = 'name';
+
   @override
   Future<List<StockListing>> getStockListings() async {
     final file = File('assets/listing_status.csv');
@@ -13,7 +15,7 @@ class StockDataSourceImpl implements StockDataSource {
         .split('\n')
         .map((e) => e.replaceAll('\r', ''))
         .map(StockListing.fromCsv)
-        .where((e) => e.name != null && e.name != '' && e.name != 'name')
+        .where((e) => e.name != null && e.name != '' && e.name != _csvLabelName)
         .toList();
   }
 }
