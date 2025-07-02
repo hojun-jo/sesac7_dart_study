adcd77ca52fc88841efe6c76c04a7ddc3944deda
Moon <1tjsen@naver.com>
2025-07-02 15:55:23 +0900

    feat: Geo 모델 추가

---
+++ lib/model/geo.dart
@@ -0,0 +1,42 @@
+class Geo {
+  final String? lat;
+  final String? lng;
+
+  Geo({
+    required this.lat,
+    required this.lng,
+  });
+
+  factory Geo.fromJson(Map<String, dynamic> json) {
+    return Geo(
+      lat: json['lat'],
+      lng: json['lng'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'lat': lat,
+      'lng': lng,
+    };
+  }
+
+  Geo copyWith({
+    String? lat,
+    String? lng,
+  }) {
+    return Geo(
+      lat: lat ?? this.lat,
+      lng: lng ?? this.lng,
+    );
+  }
+
+  @override
+  int get hashCode => Object.hash(lat, lng);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Geo && lat == other.lat && lng == other.lng;
+  }
+}
