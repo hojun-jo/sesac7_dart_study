ee997cde97ec729f7d6cd920fb4709d51a74bb88
Moon <1tjsen@naver.com>
2025-07-02 16:29:15 +0900

    feat: Address 모델 추가

---
+++ lib/model/address.dart
@@ -0,0 +1,67 @@
+import 'package:modu_3_dart_study/model/geo.dart';
+
+class Address {
+  final String? street;
+  final String? suite;
+  final String? city;
+  final String? zipcode;
+  final Geo? geo;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.geo,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      street: json['street'],
+      suite: json['suite'],
+      city: json['city'],
+      zipcode: json['zipcode'],
+      geo: json['geo'] == null ? null : Geo.fromJson(json['geo']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'street': street,
+      'suite': suite,
+      'city': city,
+      'zipcode': zipcode,
+      'geo': geo?.toJson(),
+    };
+  }
+
+  Address copyWith({
+    String? street,
+    String? suite,
+    String? city,
+    String? zipcode,
+    Geo? geo,
+  }) {
+    return Address(
+      street: street ?? this.street,
+      suite: suite ?? this.suite,
+      city: city ?? this.city,
+      zipcode: zipcode ?? this.zipcode,
+      geo: geo ?? this.geo?.copyWith(),
+    );
+  }
+
+  @override
+  int get hashCode => Object.hash(street, suite, city, zipcode, geo);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Address &&
+            street == other.street &&
+            suite == other.suite &&
+            city == other.city &&
+            zipcode == other.zipcode &&
+            geo == other.geo;
+  }
+}
