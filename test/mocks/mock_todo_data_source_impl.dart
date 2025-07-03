7fac2bed527a3e91eb67b50b44f7c074fc73e3ce
Moon <1tjsen@naver.com>
2025-07-03 18:58:21 +0900

    test: MockTodoDataSourceImpl 목객체 추가

---
+++ test/mocks/mock_todo_data_source_impl.dart
@@ -0,0 +1,35 @@
+import 'package:modu_3_dart_study/data_source/todo_data_source.dart';
+import 'package:modu_3_dart_study/model/todo.dart';
+
+class MockTodoDataSourceImpl implements TodoDataSource {
+  final jsons = [
+    {"userId": 1, "id": 1, "title": "delectus aut autem", "completed": true},
+    {
+      "userId": 2,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false,
+    },
+  ];
+  final bool? isException;
+
+  MockTodoDataSourceImpl({this.isException});
+
+  @override
+  Future<Todo> getTodo() async {
+    if (isException != null && isException!) {
+      throw Exception('Fail to get photos');
+    }
+
+    return Todo.fromJson(jsons[0]);
+  }
+
+  @override
+  Future<List<Todo>> getTodos() async {
+    if (isException != null && isException!) {
+      throw Exception('Fail to get photos');
+    }
+
+    return jsons.map(Todo.fromJson).toList();
+  }
+}
