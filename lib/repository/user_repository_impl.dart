6cd4878843bc60aa4e2683103b14393ee5182db9
Moon <1tjsen@naver.com>
2025-07-03 19:36:30 +0900

    feat: UserRepositoryImpl 구현체 추가

---
+++ lib/repository/user_repository_impl.dart
@@ -0,0 +1,44 @@
+import 'package:collection/collection.dart';
+import 'package:modu_3_dart_study/data_source/user_data_source.dart';
+import 'package:modu_3_dart_study/model/user.dart';
+import 'package:modu_3_dart_study/repository/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserDataSource _dataSource;
+
+  UserRepositoryImpl({required UserDataSource dataSource})
+    : _dataSource = dataSource;
+
+  @override
+  Future<List<User>> getUsers() async {
+    try {
+      return await _dataSource.getUsers();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  @override
+  Future<List<User>> getUsersTop10ByUserName() async {
+    try {
+      final users = await _dataSource.getUsers();
+      final sublistEnd = _calculateSublistEnd(users.length);
+      return users
+          .where((e) => e.username != null)
+          .sortedBy<String>((e) => e.username!)
+          .sublist(0, sublistEnd);
+    } catch (e) {
+      return [];
+    }
+  }
+
+  int _calculateSublistEnd(int length) {
+    int sublistEnd = 10;
+
+    if (length < sublistEnd) {
+      sublistEnd = length;
+    }
+
+    return sublistEnd;
+  }
+}
