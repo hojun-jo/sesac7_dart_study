626a36a03336a42cb5b029e2e631efa6dc7dc0d8
Moon <1tjsen@naver.com>
2025-07-06 00:02:54 +0900

    feat: PostRepositoryImpl 구현체 추가

---
+++ lib/repository/json_placeholder/post_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:modu_3_dart_study/data_source/json_placeholder/post_data_source.dart';
+import 'package:modu_3_dart_study/data_source/json_placeholder/post_data_source_impl.dart';
+import 'package:modu_3_dart_study/model/post.dart';
+import 'package:modu_3_dart_study/repository/json_placeholder/post_repository.dart';
+
+class PostRepositoryImpl implements PostRepository {
+  final PostDataSource _dataSource;
+
+  PostRepositoryImpl({required PostDataSource dataSource})
+    : _dataSource = dataSource;
+
+  @override
+  Future<List<Post>> getPostsByKeyword(String keyword) async {
+    final response = await _dataSource.getPosts();
+
+    if (!_validateStatusCode(response.statusCode)) {
+      return [];
+    }
+
+    final List jsonPosts = jsonDecode(response.body);
+    return jsonPosts
+        .map((e) => Post.fromJson(e))
+        .where((e) => e.title.contains(keyword))
+        .toList();
+  }
+
+  bool _validateStatusCode(int code) {
+    if (code >= 200 && code < 300) {
+      return true;
+    }
+    return false;
+  }
+}
