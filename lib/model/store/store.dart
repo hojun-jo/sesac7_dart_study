c87b941cdbeb92fae3f0870a4161ab8776d4d6cf
Moon <1tjsen@naver.com>
2025-07-09 20:43:00 +0900

    refactor: Store freezed 클래스 수정

--- lib/model/store/store.dart
+++ lib/model/store/store.dart
@@ -4,37 +4,18 @@ import 'package:modu_3_dart_study/model/store/store_remain_status.dart';
 part "store.freezed.dart";
 
 @freezed
-class Store with _$Store {
+abstract class Store with _$Store {
   static final unknownDate = DateTime(0);
 
-  @override
-  final String address;
-  @override
-  final String code;
-  @override
-  final DateTime createdAt;
-  @override
-  final double lat;
-  @override
-  final double lng;
-  @override
-  final String name;
-  @override
-  final StoreRemainStatus remainStatus;
-  @override
-  final DateTime stockAt;
-  @override
-  final String type;
-
-  Store({
-    required this.address,
-    required this.code,
-    required this.createdAt,
-    required this.lat,
-    required this.lng,
-    required this.name,
-    required this.remainStatus,
-    required this.stockAt,
-    required this.type,
-  });
+  const factory Store({
+    required String address,
+    required String code,
+    required DateTime createdAt,
+    required double lat,
+    required double lng,
+    required String name,
+    required StoreRemainStatus remainStatus,
+    required DateTime stockAt,
+    required String type,
+  }) = _Store;
 }
