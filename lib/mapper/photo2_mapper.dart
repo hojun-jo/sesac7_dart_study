cf725c0f79e74af8ed561dde4e74ce8c16b94d10
Moon <1tjsen@naver.com>
2025-07-09 18:04:40 +0900

    feat: photo2 mapper 추가

---
+++ lib/mapper/photo2_mapper.dart
@@ -0,0 +1,53 @@
+import 'package:intl/intl.dart';
+import 'package:modu_3_dart_study/dto/photo2/photo2_dto.dart';
+import 'package:modu_3_dart_study/model/photo2/photo2.dart';
+import 'package:modu_3_dart_study/model/photo2/photo2_type.dart';
+
+extension Photo2DtoToModel on Photo2Dto {
+  Photo2 toModel() {
+    return Photo2(
+      id: id ?? 0,
+      type: _stringToType(type),
+      title: title ?? '',
+      content: content ?? '',
+      url: url ?? '',
+      caption: caption ?? '',
+      createdAt: _stringToDateTime(createdAt),
+    );
+  }
+
+  Photo2Type _stringToType(String? value) {
+    return switch (value) {
+      'article' => Photo2Type.article,
+      'image' => Photo2Type.image,
+      'video' => Photo2Type.video,
+      _ => Photo2Type.unknown,
+    };
+  }
+
+  DateTime _stringToDateTime(String? value) {
+    if (value == null || value == '') {
+      return Photo2.unknownDate;
+    }
+
+    return DateTime.parse(value);
+  }
+}
+
+extension Photo2ModelToDto on Photo2 {
+  Photo2Dto toDto() {
+    return Photo2Dto(
+      id: id,
+      type: type.toString(),
+      title: title,
+      content: content,
+      url: url,
+      caption: caption,
+      createdAt: _formatDate(createdAt),
+    );
+  }
+
+  String _formatDate(DateTime value) {
+    return DateFormat('yyyy-MM-dd').format(value);
+  }
+}
