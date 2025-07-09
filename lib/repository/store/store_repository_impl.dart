bbb438f5ac761d84de5af727d8795978f8a237e7
Moon <1tjsen@naver.com>
2025-07-09 16:31:23 +0900

    feat: StoreRepositoryImpl 추가

---
+++ lib/repository/store/store_repository_impl.dart
@@ -0,0 +1,43 @@
+import 'package:modu_3_dart_study/core/network_validator.dart';
+import 'package:modu_3_dart_study/data_source/store/store_data_source.dart';
+import 'package:modu_3_dart_study/mapper/store_mapper.dart';
+import 'package:modu_3_dart_study/model/store/store.dart';
+import 'package:modu_3_dart_study/model/store/store_remain_status.dart';
+import 'package:modu_3_dart_study/repository/store/store_repository.dart';
+
+class StoreRepositoryImpl implements StoreRepository {
+  final StoreDataSource _dataSource;
+  final NetworkValidator _validator;
+
+  StoreRepositoryImpl({
+    required StoreDataSource dataSource,
+    NetworkValidator? validator,
+  }) : _dataSource = dataSource,
+       _validator = validator ?? NetworkValidator();
+
+  @override
+  Future<List<Store>> getStores() async {
+    try {
+      final response = await _dataSource.getStores();
+
+      if (!_validator.validateStatusCode(response.statusCode)) {
+        return [];
+      }
+      if (response.body.stores == null) {
+        return [];
+      }
+
+      return response.body.stores!
+          .map((dto) => dto.toModel())
+          .where(
+            (e) =>
+                e.remainStatus != StoreRemainStatus.unknown &&
+                e.stockAt != Store.unknownDate &&
+                e.createdAt != Store.unknownDate,
+          )
+          .toList();
+    } catch (e) {
+      return [];
+    }
+  }
+}
