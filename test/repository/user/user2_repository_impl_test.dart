0c7e0fed6d0031cb6dd9e37346284aa748618bcb
Moon <1tjsen@naver.com>
2025-07-10 18:59:22 +0900

    test: User2RepositoryImpl 테스트 추가

---
+++ test/repository/user/user2_repository_impl_test.dart
@@ -0,0 +1,302 @@
+import 'dart:async';
+
+import 'package:modu_3_dart_study/core/network_error.dart';
+import 'package:modu_3_dart_study/core/result.dart';
+import 'package:modu_3_dart_study/data_source/user/user2_data_source.dart';
+import 'package:modu_3_dart_study/model/user/user2.dart';
+import 'package:modu_3_dart_study/repository/user/user2_repository.dart';
+import 'package:modu_3_dart_study/repository/user/user2_repository_impl.dart';
+import 'package:test/test.dart';
+
+import '../../mocks/mock_user2_data_source_impl.dart';
+
+void main() {
+  final User2DataSource dataSource = MockUser2DataSourceImpl(statusCode: 200);
+  final User2Repository repository = User2RepositoryImpl(
+    dataSource: dataSource,
+  );
+  final userId = 1;
+  final user = User2(id: userId, name: 'name1');
+
+  group(
+    'createUser',
+    () {
+      test(
+        'If the statusCode is 200, return the User2 that performed the create.',
+        () async {
+          // given
+
+          // when
+          final result = await repository.createUser(user);
+
+          // then
+          expect(result, Result<User2, NetworkError>.success(user));
+        },
+      );
+
+      test(
+        'Returns NetworkError.notFound if statusCode is 404.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 404,
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.createUser(user);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.notFound),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.requestTimeout if a TimeoutException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: TimeoutException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.createUser(user);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.requestTimeout),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.parseError if a FormatException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: FormatException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.createUser(user);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.parseError),
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'getUser',
+    () {
+      test(
+        'Returns User2 if a user with the matching id exists.',
+        () async {
+          // given
+
+          // when
+          final result = await repository.getUser(userId);
+
+          // then
+          expect(result, Result<User2, NetworkError>.success(user));
+        },
+      );
+
+      test(
+        'Returns NetworkError.unknown if no user with a matching id exists.',
+        () async {
+          // given
+          final wrongId = -1;
+
+          // when
+          final result = await repository.getUser(wrongId);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.unknown),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.notFound if statusCode is 404.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 404,
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUser(userId);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.notFound),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.requestTimeout if a TimeoutException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: TimeoutException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUser(userId);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.requestTimeout),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.parseError if a FormatException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: FormatException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUser(userId);
+
+          // then
+          expect(
+            result,
+            Result<User2, NetworkError>.error(NetworkError.parseError),
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'getUsers',
+    () {
+      test(
+        'If the statusCode is 200, return the User2 list.',
+        () async {
+          // given
+          final users = [
+            User2(id: 1, name: 'name1'),
+            User2(id: 2, name: 'name2'),
+            User2(id: 3, name: 'name3'),
+          ];
+
+          // when
+          final result = await repository.getUsers();
+
+          // then
+          expect(result, Result<List<User2>, NetworkError>.success(users));
+        },
+      );
+
+      test(
+        'Returns NetworkError.notFound if statusCode is 404.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 404,
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUsers();
+
+          // then
+          expect(
+            result,
+            Result<List<User2>, NetworkError>.error(NetworkError.notFound),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.requestTimeout if a TimeoutException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: TimeoutException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUsers();
+
+          // then
+          expect(
+            result,
+            Result<List<User2>, NetworkError>.error(
+              NetworkError.requestTimeout,
+            ),
+          );
+        },
+      );
+
+      test(
+        'Returns NetworkError.parseError if a FormatException is thrown.',
+        () async {
+          // given
+          final User2DataSource dataSource = MockUser2DataSourceImpl(
+            statusCode: 200,
+            exception: FormatException('message'),
+          );
+          final User2Repository repository = User2RepositoryImpl(
+            dataSource: dataSource,
+          );
+
+          // when
+          final result = await repository.getUsers();
+
+          // then
+          expect(
+            result,
+            Result<List<User2>, NetworkError>.error(NetworkError.parseError),
+          );
+        },
+      );
+    },
+  );
+}
