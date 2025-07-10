e20685a7cd6a8c53ee1fc954cbdc0482a86e102d
Moon <1tjsen@naver.com>
2025-07-10 16:32:10 +0900

    fix: User2DataSource 인터페이스 수정

--- lib/data_source/user/user2_data_source.dart
+++ lib/data_source/user/user2_data_source.dart
@@ -2,6 +2,6 @@ import 'package:modu_3_dart_study/core/response.dart';
 import 'package:modu_3_dart_study/dto/user/user2_dto.dart';
 
 abstract interface class User2DataSource {
-  Future<Response<User2Dto>> getUsers();
+  Future<Response<List<User2Dto>>> getUsers();
   Future<Response<User2Dto>> createUser(User2Dto user);
 }
