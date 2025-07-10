e5a28a36b83c1605b4c5492c998cab018d90b7c4
Moon <1tjsen@naver.com>
2025-07-10 16:19:34 +0900

    feat: User2Dto 구현

--- lib/dto/user/user2_dto.dart
+++ lib/dto/user/user2_dto.dart
@@ -1,1 +1,19 @@
-class User2Dto {}
+class User2Dto {
+  int? id;
+  String? name;
+
+  User2Dto({
+    this.id,
+    this.name,
+  });
+
+  factory User2Dto.fromJson(Map<String, dynamic> json) => User2Dto(
+    id: json['id'],
+    name: json['name'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+  };
+}
